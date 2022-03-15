5a4e203b9ccdf8adf72fe60a1a9d341e55e624f3
Imad Sheriff <imadsheriff97@gmail.com>
2022-03-15 01:47:38 -0400

    Added Build Step for base image before project

--- build/Docker/Scripts/app-dockerbuild.sh
+++ build/Docker/Scripts/app-dockerbuild.sh
@@ -1,3 +1,5 @@
 echo "Building Base App Docker Image"
 docker build --rm -t app-base-image:latest -f build/Docker/Dockerfiles/app-Dockerfile .
+docker tag app-base-image:latest registry.gitlab.com/imadsheriff97/pokehub-app/app-base-image:latest
+docker push registry.gitlab.com/imadsheriff97/pokehub-app/app-base-image:latest
 echo "Done building Base App Docker Image"
