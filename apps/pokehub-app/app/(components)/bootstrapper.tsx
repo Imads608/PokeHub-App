2e46aae898f0390c25aca34a1e543ab6716493b7
Imad Sheriff <imadsheriff97@gmail.com>
2025-11-19 19:16:05 -0800

    ITEM-79: Deployment Docs
    Added Docs on Deployment and Build

--- apps/pokehub-app/app/(components)/bootstrapper.tsx
+++ apps/pokehub-app/app/(components)/bootstrapper.tsx
@@ -1,13 +1,10 @@
 'use client';
 
 import { App } from './app';
-import type { ModdedDex } from '@pkmn/dex';
-import { Dex } from '@pkmn/dex';
 import {
   SharedAppBootstrapper,
   type Provider,
 } from '@pokehub/frontend/shared-app-bootstrapper';
-import { AuthContextProvider } from '@pokehub/frontend/shared-auth-provider';
 import { createFetchClient } from '@pokehub/frontend/shared-data-provider';
 import { QueryProvider } from '@pokehub/frontend/shared-query-client-provider';
 import { ThemeProvider } from '@pokehub/frontend/shared-theme-context';
@@ -19,13 +16,12 @@ export const AppBootstrapper = ({
   children: React.ReactNode;
 }) => {
   const providers: Provider[] = useMemo(
-    () => [QueryProvider, AuthContextProvider, ThemeProvider],
+    () => [QueryProvider, ThemeProvider],
     []
   );
 
   useEffect(() => {
     createFetchClient('API', process.env.NEXT_PUBLIC_POKEHUB_API_URL);
-    (window as unknown as { Dex: ModdedDex }).Dex = Dex;
   }, []);
 
   return (
