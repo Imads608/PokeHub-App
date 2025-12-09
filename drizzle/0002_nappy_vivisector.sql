623cc104cd1e0e56e9e1aa537e6eb24549ef5633
Imads608 <imadsheriff97@gmail.com>
2025-12-08 23:57:26 -0800

    Fix migrations

--- drizzle/0002_nappy_vivisector.sql
+++ drizzle/0002_nappy_vivisector.sql
@@ -1,1 +1,6 @@
-ALTER TABLE "users" RENAME COLUMN "avatarPath" TO "avatarFilename";
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'users' AND column_name = 'avatarPath') THEN
+        ALTER TABLE "users" RENAME COLUMN "avatarPath" TO "avatarFilename";
+    END IF;
+END $$;
