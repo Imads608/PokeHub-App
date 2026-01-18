f8fbf842d84391e2a6e40ce2b07a863f71db14ef
Imads608 <imadsheriff97@gmail.com>
2026-01-18 11:05:46 -0800

    ITEM-125: Static Content Fix

--- apps/pokehub-app/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <div className="h-16 w-16 animate-spin rounded-full border-4 border-t-4 border-gray-200 border-t-blue-500"></div>
-    </div>
-  );
-}
